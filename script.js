d52c1b0adff070c629d514fa941a8f83ce752987
Arnika22 <121330860+Arnika22@users.noreply.github.com>
2024-03-18 04:27:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let slide = document.querySelectorAll(".customer");
+let count = 0;
+console.log(slide);
+
+slide.forEach(function(customer, index){
+    customer.style.left=`${index * 100}%`
+})
+
+function next(){
+    count ++;
+    if(count == slide.length){
+        count=0;
+    }
+    bar()
+}
+
+function pre(){
+    count--;
+    if(count == -1){
+        count = slide.length-1
+    }
+    bar()
+}
+
+
+function bar(){
+    slide.forEach(function(customer){
+        customer.style.transform = `translateX(-${count * 100}%)`
+    })
+}
+
